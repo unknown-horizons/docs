3d7fd11b98d09fdc45e6602f5817617676bf124c
Thomas Kowaliczek-Schmer <linuxdonald@posteo.de>
2021-06-11 00:41:52 +0200

    update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Unknown Horizons'
-copyright = '2012-2017, squiddy'
+copyright = '2012-2021, squiddy, Unknown-Horizons Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
